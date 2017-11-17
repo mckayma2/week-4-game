4f819733f881ade333ca17775ff21c25717929fd
Built-in Administrator <localadmin@USA-MIA-L006.marineharvest.net>
2017-11-17 09:57:28 -0500

    final upl

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,16 @@ $( document ).ready(function() {
 // main variables
 //==================================================
 
-var playerGuess;
-var playerRandomNumber;
-var playerTotalScore = 0;
-var playerScore;
-var score;
+
 var scoreCounter;
-var randomNumberMatch;
 var myarr = [];
-var crystalValue;
+var playerScore;
 var sum=0;
 var loses=0;
+var wins=0;
+var playerTotalScore;
+var loses=0;
+wins=0;
 //random number generators
 //===================================================
 
@@ -24,57 +23,57 @@ var loses=0;
 
 //functions
 //==================================================
+function computerGuess(){
+scoreCounter = Math.floor(Math.random() * (121 - 19)+19);
+return scoreCounter;
+};
 
-crystalValue = function(){
-playerScore = Math.floor((Math.random() * 12)+1);
+function crystalValue(){
+playerScore = Math.floor(Math.random()* (12 +1));
 return playerScore;
-console.log(playerScore);
-
-}
-
-playerGuess = function (min, max) {
-  min = Math.ceil(19);
-  max = Math.floor(121);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+};
 
 
-}
-console.log("PlayerGuess	:" + playerGuess());
 
-function game(){
+console.log("player score " + crystalValue());
+console.log("computer Guess " + computerGuess());
 
-console.log(playerScore);
-$("#randNum").html("Random Number  :" + playerGuess());
 
 $( ".im" ).click(function() {
-	$(".als").html(crystalValue());
-	myarr.push(playerScore);
-	console.log(myarr);
-	playerTotalScore = myarr.reduce(function(accumulator, currentValue) {
+var a = playerTotalScore;
+var b = scoreCounter;
+if(a>b){
+scoreCounter = 0;
+scoreCounter = Math.floor(Math.random() * (121 - 19)+19);
+$("#randNum").html(scoreCounter);
+loses++;
+playerTotalScore=0;
+myarr=[];
+alert("You lost ---Your Total score exceeded the computer Guess")
+}
+else if(a===b){
+wins++;
+scoreCounter = 0;
+scoreCounter = Math.floor(Math.random() * (121 - 19)+19);
+$("#randNum").html(scoreCounter);
+myarr=[];
+alert("You win --- Your Total score matched the computer Guess") 
+}
+$(".als").html(crystalValue());
+myarr.push(playerScore);
+console.log("myArr " + myarr);
+playerTotalScore = myarr.reduce(function(accumulator, currentValue) {
     return accumulator + currentValue;});
-	score = playerTotalScore;
-	console.log ("playerTotalScore  "+ playerTotalScore)
-$(".als2").html("Total Score  :" + playerTotalScore + "   Score  : " + score + "  Loses:  " + loses);
+console.log("playerTotalScore " + playerTotalScore);
 
 
-if(playerTotalScore>119){
-loses+=1;
-score=0;
-}
+$(".als2").html("Total Score  :  " + playerTotalScore +   " Wins  :  " + wins +   "  Loses  :  " + loses);
 });
-
-
-}
 
 //DOM
 //================================================
 
-
-	
-
-game();
-$("#randNum").html("Random Number  :" + playerGuess());
-
-
+$("#randNum").html(scoreCounter);
 
 });
+
